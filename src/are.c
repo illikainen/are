533cae65cd3531bd62a5377d4ffa3f8a74618852
Hans Jerry Illikainen <hji@dyntopia.com>
2019-08-20 15:32:34 +0000

    are: are_string_match(): dispatch to engine implementation

--- src/are.c
+++ src/are.c
@@ -45,16 +45,51 @@ static struct are_engine *are_find_engine(emacs_env *env)
 static emacs_value are_string_match(emacs_env *env, ptrdiff_t nargs,
                                     emacs_value args[], void *data)
 {
-    struct string *regexp;
+    struct are_engine *engine;
+    struct string *regexp = NULL;
+    struct string *str = NULL;
+    intmax_t start = 0;
+    emacs_value rv = env->intern(env, "nil");
 
-    (void)nargs;
     (void)data;
 
-    regexp = extract_string(env, args[0]);
-    printf("regexp: %s\n", regexp->str);
-    free_string(regexp);
+    if (nargs < 2) {
+        goto out;
+    }
 
-    return env->intern(env, "string-match");
+    regexp = extract_string(env, args[0]);
+    if (regexp == NULL) {
+        non_local_exit_signal(env, "Invalid regexp");
+        goto out;
+    }
+
+    str = extract_string(env, args[1]);
+    if (str == NULL) {
+        non_local_exit_signal(env, "Invalid string");
+        goto out;
+    }
+
+    if (nargs > 2 && is_integer(env, args[2])) {
+        start = extract_integer(env, args[2]);
+        if (start < 0 || start > str->len) {
+            non_local_exit_signal(env, "Invalid start position");
+            goto out;
+        }
+    }
+
+    engine = are_find_engine(env);
+    if (engine == NULL || engine->string_match == NULL) {
+        non_local_exit_signal(env, "Invalid engine");
+        goto out;
+    }
+
+    debug("regexp: '%s', str: '%s', start: %d", regexp->str, str->str, start);
+    rv = engine->string_match(env, regexp, str, start);
+
+out:
+    free_string(regexp);
+    free_string(str);
+    return rv;
 }
 module_register_fun(are_string_match, "are-string-match", 1, 4,
                     "ARE version of `re-search-forward'.");
