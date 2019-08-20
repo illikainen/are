95d9e3ffc12ebf8d83431f54d605929590cc54d9
Hans Jerry Illikainen <hji@dyntopia.com>
2019-08-20 10:33:04 +0000

    are: add support for registering engines

--- src/are.c
+++ src/are.c
@@ -10,7 +10,8 @@
 
 #include "are.h"
 #include "module.h"
-#include "types.h"
+
+static struct are_engine *engines = NULL;
 
 static emacs_value are_string_match(emacs_env *env, ptrdiff_t nargs,
                                     emacs_value args[], void *data)
@@ -28,3 +29,11 @@ static emacs_value are_string_match(emacs_env *env, ptrdiff_t nargs,
 }
 module_register_fun(are_string_match, "are-string-match", 1, 4,
                     "ARE version of `re-search-forward'.");
+
+/*
+ * Add an engine.
+ */
+void are_add_engine(struct are_engine *engine)
+{
+    HASH_ADD_KEYPTR(hh, engines, engine->name, strlen(engine->name), engine);
+}
--- src/are.h
+++ src/are.h
@@ -7,4 +7,24 @@
 #ifndef ARE_H
 #define ARE_H
 
+#include <uthash.h>
+
+#include "types.h"
+
+struct are_engine {
+    const char *name;
+    emacs_value (*string_match)(emacs_env *env, struct string *regexp,
+                                struct string *str, intmax_t start);
+    UT_hash_handle hh;
+};
+
+#define are_register_engine(engine)                                            \
+    static void __attribute__((constructor)) are_register_##engine(void)       \
+    {                                                                          \
+        engine.name = #engine;                                                 \
+        are_add_engine(&engine);                                               \
+    }
+
+void are_add_engine(struct are_engine *engine);
+
 #endif
