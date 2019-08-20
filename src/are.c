251647c962d8848169083bf07a77e6b2c40e5e63
Hans Jerry Illikainen <hji@dyntopia.com>
2019-08-20 10:37:44 +0000

    are: add are_find_engine()

--- src/are.c
+++ src/are.c
@@ -9,9 +9,38 @@
 #include <emacs-module.h>
 
 #include "are.h"
+#include "log.h"
 #include "module.h"
 
 static struct are_engine *engines = NULL;
+
+static struct are_engine *are_find_engine(emacs_env *env)
+{
+    struct are_engine *engine = NULL;
+    struct string *name = NULL;
+    emacs_value value;
+
+    value = value_of(env, "are-engine");
+    if (!env->is_not_nil(env, value)) {
+        debug("are-engine is not set");
+        return NULL;
+    }
+
+    value = funcall(env, "symbol-name", 1, value);
+    if (!env->is_not_nil(env, value)) {
+        return NULL;
+    }
+
+    name = extract_string(env, value);
+    if (name == NULL) {
+        return NULL;
+    }
+
+    HASH_FIND_STR(engines, name->str, engine);
+    free_string(name);
+
+    return engine;
+}
 
 static emacs_value are_string_match(emacs_env *env, ptrdiff_t nargs,
                                     emacs_value args[], void *data)
