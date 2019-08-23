c1cba70e72c85a1d422799ba3e89dfcbf8e12125
Hans Jerry Illikainen <hji@dyntopia.com>
2019-08-23 21:21:28 +0000

    are.el: implement are-re-search-forward

--- src/are.el
+++ src/are.el
@@ -93,6 +93,89 @@ See `string-match'."
     (are--set-match-data mdata)
     (car mdata)))
 
+(defun are-re-search-forward (regexp &optional bound noerror count)
+  "Search forward from `point' for REGEXP.
+
+See `re-search-forward'."
+  (let* ((re (are-compile regexp))
+         (cnt (or count 1))
+         (start (cond ((> cnt 0)
+                       (point))
+                      ((not bound)
+                       (point-min))
+                      (t
+                       (max (point-min) bound))))
+         (end (cond ((and (> cnt 0) (not bound))
+                     (point-max))
+                    ((and (<= cnt 0) (not bound))
+                     (point-max))
+                    ((and (> cnt 0) (< bound (point)))
+                     (signal 'args-out-of-range (list bound)))
+                    ((and (<= cnt 0) (> bound (point)))
+                     (signal 'args-out-of-range (list bound)))
+                    ((and (= cnt 0) (< bound (point-min)))
+                     (point-min))
+                    ((> cnt 0)
+                     (1- (+ (point) bound)))
+                    ((<= cnt 0)
+                     (1- (point)))))
+         (str (buffer-substring start end))
+         (pt (ignore-errors
+               (cond ((> cnt 0)
+                      (cadr (are--re-search-forward-forward re str cnt)))
+                     ((< cnt 0)
+                      (save-excursion
+                        (goto-char start)
+                        (car (are--re-search-forward-backward re str cnt))))
+                     ((eq cnt 0)
+                      ;; If COUNT is 0, `re-search-forward' validates the bound
+                      ;; and signals if it's invalid.  However, no actual
+                      ;; search seem to take place.  Instead, the start and end
+                      ;; positions of `match-data' is set to `point', and
+                      ;; `point' is returned..
+                      (set-match-data (list (set-marker (make-marker) (point))
+                                            (set-marker (make-marker) (point))))
+                      (point))
+                      ))))
+    (if pt
+        (goto-char pt)
+      (cl-case noerror
+        ((nil) (signal 'search-failed (list regexp)))
+        ((t) nil)
+        (otherwise
+         (if (> cnt 0)
+             (goto-char end)
+           (goto-char start)))))
+    pt))
+
+(defun are--re-search-forward-forward (re str count)
+  "Do a forward search with `are-re-search-forward'."
+  (let ((offset 0)
+        mdata)
+    (dotimes (n count)
+      (setq mdata (are-match re (substring str offset)))
+      (unless mdata
+        (signal 'search-failed '()))
+      (setq mdata (are--adjust-match-data mdata (+ (point) offset)))
+      (setq offset (cadr mdata))
+      (are--set-match-data mdata t))
+    mdata))
+
+(defun are--re-search-forward-backward (re str count)
+  "Do a backward search with `are-re-search-forward'."
+  (let* ((start (length str))
+         (end start)
+         mdata)
+    (while (and (< count 0) (> start 0))
+      (setq mdata (are-match re (substring str start end)))
+      (when mdata
+        (setq mdata (are--adjust-match-data mdata (+ (point) start)))
+        (setq end start)
+        (are--set-match-data mdata t)
+        (cl-incf count))
+      (cl-decf start))
+    mdata))
+
 (provide 'are)
 
 ;;; are.el ends here
